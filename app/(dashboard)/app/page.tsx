b1144492a7602b321b97f4ee7b0a9d84665e6ecb
Dermot <contact@dermotobrien.me>
2023-11-17 22:47:25 +0000

    📹 feat: Implement Video Library in Dashboard with Prisma
    Introduced a new feature, Video Library, in the Dashboard page. This enhancement enables users to view and manage their uploaded videos. The page fetches video data from the database using Prisma and displays each video with a thumbnail, title, upload date, and view count. This feature provides users with an intuitive and easy way to access and interact with their video content.
    
    Included authentication checks to ensure only logged-in users can access the Dashboard. If a user is not authenticated, they are redirected to the signin page. This addition enhances the security of the application by restricting access to sensitive user data.
    
    Integrated 'date-fns' library for formatting video upload dates. This library allows the application to display human-readable time distances, such as "2 days ago," improving the user experience by providing a clearer context of when videos were uploaded.
    
    Overall, these changes significantly enhance the user experience of the Dashboard, making it a central hub for users to manage and view their video content efficiently.

--- app/(dashboard)/app/page.tsx
+++ app/(dashboard)/app/page.tsx
@@ -1,7 +1,123 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/AuthOptions";
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { formatDistanceToNow } from "date-fns";
+import Link from "next/link";
+
 export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/signin?redirect=/app");
+  }
+
+  const prisma = new PrismaClient();
+  // @ts-ignore
+  const userId = session?.user?.id;
+  if (!userId) return redirect("/signin");
+  const videos = await prisma.upload.findMany({
+    where: {
+      // @ts-ignore
+      User: {
+        id: userId,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <section className="relative">
-      <div className="relative max-w-6xl mx-auto px-4 sm:px-6">Dashboard</div>
+      <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-32 md:pt-40">
+          <div
+            key="1"
+            className="flex flex-col bg-slate-800 bg-opacity-60 rounded overflow-hidden"
+          >
+            <header className="bg-slate-800 bg-opacity-60 rounded overflow-hidden">
+              <div className="max-w-7xl mx-auto flex items-center justify-between p-4">
+                <h1 className="text-lg font-semibold">Video Library</h1>
+              </div>
+            </header>
+            <main className="flex-1 p-4 bg-slate-800 bg-opacity-60 rounded overflow-hidden">
+              <div className="grid grid-cols-3 gap-4">
+                {videos.map((video) => {
+                  const previewUrl = `https://image.mux.com/${video?.playbackId}/thumbnail.png?width=1080&height=720&time=0`;
+                  // const animatedPreviewUrl = `https://image.mux.com/${video?.playbackId}/animated.gif`;
+
+                  return (
+                    <div className="group" key={video.id}>
+                      <Link href={`/view/${video.id}`}>
+                        <div className="relative aspect-w-16 aspect-h-9 bg-slate-800  bg-opacity-60 ">
+                          <img
+                            alt=""
+                            className="object-cover"
+                            height="720"
+                            src={previewUrl}
+                            style={{
+                              aspectRatio: "1280/720",
+                              objectFit: "cover",
+                            }}
+                            width="1280"
+                          />
+                          <div className="absolute inset-0 flex items-center justify-center">
+                            <button className="group-hover:scale-105 transition-transform group-hover:text-gray-600">
+                              <svg
+                                className="h-8 w-8 text-gray-300"
+                                fill="none"
+                                height="24"
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                                width="24"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <polygon points="5 3 19 12 5 21 5 3" />
+                              </svg>
+                              <span className="sr-only">Play video</span>
+                            </button>
+                          </div>
+                        </div>
+                      </Link>
+                      <h2 className="mt-2 text-sm font-semibold">
+                        {video?.sourceTitle} Recording
+                      </h2>
+                      <div className="flex items-center justify-between  pt-2 rounded">
+                        <div className="text-xs">
+                          {formatDistanceToNow(new Date(video.createdAt))} ago
+                        </div>
+                        <div className="text-xs">0 views</div>
+                        <button>
+                          <svg
+                            className=" h-6 w-6"
+                            fill="none"
+                            height="24"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" />
+                            <polyline points="16 6 12 2 8 6" />
+                            <line x1="12" x2="12" y1="2" y2="15" />
+                          </svg>
+                          <span className="sr-only">Share video</span>
+                        </button>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
