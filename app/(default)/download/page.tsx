a579371fecddd91272f505d8ca2fafa837ad5201
Dermot <contact@dermotobrien.me>
2023-11-18 00:17:08 +0000

    ✨ feat: Add dynamic OS detection for download page
    Implemented a new download page that dynamically detects the user's operating system. This feature enhances the user experience by automatically suggesting the correct version of the application for the user's OS. The `getOS` function utilizes the `window.navigator.userAgent` string to identify whether the user is on macOS, Windows, or Linux, and then adjusts the download link accordingly.
    
    This update streamlines the download process for users, making it more intuitive and user-friendly. By automatically detecting the operating system, it eliminates the need for users to manually select the correct version, reducing the possibility of downloading the wrong file and improving overall user satisfaction with the application.

---
+++ app/(default)/download/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import Link from "next/link";
+
+function getOS() {
+  const userAgent = window.navigator.userAgent,
+    macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
+    windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"];
+  let os = null;
+
+  if (macosPlatforms.some((platform) => userAgent.includes(platform))) {
+    os = "macOS";
+  } else if (
+    windowsPlatforms.some((platform) => userAgent.includes(platform))
+  ) {
+    os = "Windows";
+  } else if (/Linux/.test(userAgent)) {
+    os = "Linux";
+  }
+
+  return os;
+}
+
+export default function Download() {
+  const os = getOS();
+
+  return (
+    <section className="w-full py-12 md:py-24 lg:py-32 min-h-screen flex items-center justify-center">
+      <div className="container px-4 md:px-6">
+        <div className="flex flex-col items-center space-y-4">
+          <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl dark:text-white">
+            Download ScreenLink
+          </h1>
+          <p className="max-w-[600px] text-zinc-500 text-center md:text-sm/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-zinc-400">
+            ScreenLink allows you to record your screen and share it with
+            anyone! Download ScreenLink and get started for free today.
+          </p>
+        </div>
+        <div className="flex justify-center mt-12">
+          <Link
+            className="inline-flex items-center rounded-md bg-blue-400/10 px-3 py-3 text-2xl font-medium text-blue-400 ring-1 ring-inset ring-blue-400/30 hover:bg-blue-400/20 dark:bg-blue-400/20 dark:text-blue-400 dark:ring-blue-400/20"
+            href="#"
+          >
+            Download for {os}
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
