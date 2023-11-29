3196d4f329cb456c93c355a7596f63be977ecd2f
Dermot <contact@dermotobrien.me>
2023-11-29 19:04:08 +0000

    🎨 style: update HTML title from 'ScreenLink.io' to 'ScreenLink' 📦 feat: add new dependencies to package.json for enhanced UI features 🔧 refactor: update tailwind.config.js to include new plugin
    The title in the HTML file has been updated to 'ScreenLink', making it more concise and clear. This change reflects a streamlined branding approach. New dependencies have been added to the package.json, including '@headlessui/react', '@tailwindcss/line-clamp', and 'lucide-react', to enhance the user interface with more modern, efficient, and visually appealing components. The Tailwind configuration file has been refactored to include the new '@tailwindcss/line-clamp' plugin, enabling better text handling and display within the UI.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Path to your JS, JSX, TS, and TSX files
+    "./src/**/*.{js,jsx,ts,tsx}", // Path to your JS, JSX, TS, and TSX files
     // './public/index.html', // Path to your HTML files
     // Add paths to any other HTML, JS, JSX, TS, or TSX files
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
