adf528211c7e3aed982d1323b526e915b775b886
Dermot <contact@dermotobrien.me>
2023-12-15 22:47:09 +0000

    🚀 feat: refactor DownloadButton, add macOS specific downloads, remove unused import
    This commit enhances the DownloadButton component in 'DownloadButton.tsx' by introducing several improvements. Firstly, the local getOS function is replaced with an imported version from '@/app/utils', ensuring better code reusability and maintainability. This change aligns with best practices in software development by reducing duplication and leveraging shared utilities. Secondly, the commit introduces specialized download buttons for macOS users, catering to both Intel and Apple Silicon processors. This feature enhances user experience by providing tailored download options that acknowledge the hardware diversity among macOS users.

--- app/(default)/download/DownloadButton.tsx
+++ app/(default)/download/DownloadButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { getOS } from "@/app/utils";
 import { HeaderAction } from "@/components/HeaderAction";
-import { Link } from "lucide-react";
 import { useEffect, useState } from "react";
 
 // Define the interface for a GitHub release asset
@@ -37,30 +37,11 @@ function getDownloadUrl(os: string, releases: GitHubRelease[]): string | null {
   return null;
 }
 
-function getOS(): string {
-  if (typeof window === "undefined") return "Unknown";
-  const userAgent = window.navigator.userAgent,
-    macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
-    windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"];
-  let os = "Unknown";
-
-  if (macosPlatforms.some((platform) => userAgent.includes(platform))) {
-    os = "macOS";
-  } else if (
-    windowsPlatforms.some((platform) => userAgent.includes(platform))
-  ) {
-    os = "Windows";
-  } else if (/Linux/.test(userAgent)) {
-    os = "Linux";
-  }
-
-  return os;
-}
-
 export const DownloadButton = () => {
   const [os, setOs] = useState<string | null>(null);
   const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
   const [githubRequestFinished, setGithubRequestFinished] = useState(false);
+  const [releases, setReleases] = useState<GitHubRelease[]>([]);
 
   useEffect(() => {
     const updatedOs = getOS();
@@ -75,6 +56,7 @@ export const DownloadButton = () => {
         // Assuming the first release in the array is the latest
         const latestRelease = releases[0];
         const url = getDownloadUrl(updatedOs, releases);
+        setReleases(releases);
         setDownloadUrl(url);
         setGithubRequestFinished(true);
       })
@@ -89,8 +71,11 @@ export const DownloadButton = () => {
       <div className="flex flex-col items-center justify-center h-full">
         <p className="max-w-[800px] mb-4 text-white text-center md:text-sm/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-white">
           ScreenLink is being built for MacOS, Windows and Linux. Its still in
-          development {os ? `but its not yet available for ${os}!` : "."} Get
-          notified when it is available
+          development
+          {os && os !== "Unknown"
+            ? ` but its not yet available for ${os}!`
+            : "."}{" "}
+          Get notified when it is available for your OS.
         </p>
         <HeaderAction />
       </div>
@@ -106,6 +91,9 @@ export const DownloadButton = () => {
     return (
       <span className="animate-pulse inline-flex items-center rounded-md bg-blue-400/10 px-3 py-3 text-2xl font-medium text-blue-400 ring-1 ring-inset ring-blue-400/30 hover:bg-blue-400/20 dark:bg-blue-400/20 dark:text-blue-400 dark:ring-blue-400/20 w-44 h-16 cursor-pointer"></span>
     );
+
+  if (os === "macOS") return <MacDownloadButton releases={releases} />;
+
   return (
     <a
       href={downloadUrl ?? "#"}
@@ -114,5 +102,49 @@ export const DownloadButton = () => {
     >
       Download for {os}
     </a>
+  );
+};
+
+const MacDownloadButton = ({ releases }: { releases: GitHubRelease[] }) => {
+  const intelMacPattern = /ScreenLink-\d+\.\d+\.\d+\.dmg/;
+  const armMacPattern = /ScreenLink-\d+\.\d+\.\d+-arm64\.dmg/;
+
+  const intelMacRelease = releases.find((release) =>
+    release.assets.find((asset) => intelMacPattern.test(asset.name))
+  );
+
+  const armMacRelease = releases.find((release) =>
+    release.assets.find((asset) => armMacPattern.test(asset.name))
+  );
+
+  return (
+    <div className="flex flex-row items-center justify-center h-full space-x-4">
+      {intelMacRelease && (
+        <a
+          href={
+            intelMacRelease?.assets.find((asset) =>
+              intelMacPattern.test(asset.name)
+            )?.browser_download_url ?? "#"
+          }
+          className="inline-flex items-center rounded-md bg-blue-400/10 px-3 py-3 text-2xl font-medium text-blue-400 ring-1 ring-inset ring-blue-400/30 hover:bg-blue-400/20 dark:bg-blue-400/20 dark:text-blue-400 dark:ring-blue-400/20"
+          download
+        >
+          Download for Intel
+        </a>
+      )}
+      {armMacRelease && (
+        <a
+          href={
+            armMacRelease?.assets.find((asset) =>
+              armMacPattern.test(asset.name)
+            )?.browser_download_url ?? "#"
+          }
+          className="inline-flex items-center rounded-md bg-blue-400/10 px-3 py-3 text-2xl font-medium text-blue-400 ring-1 ring-inset ring-blue-400/30 hover:bg-blue-400/20 dark:bg-blue-400/20 dark:text-blue-400 dark:ring-blue-400/20"
+          download
+        >
+          Download for Apple Silicon
+        </a>
+      )}
+    </div>
   );
 };
