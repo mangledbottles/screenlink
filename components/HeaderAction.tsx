9a18f1267c1fe44db8ef6f50954015ef68be52f8
Dermot <contact@dermotobrien.me>
2023-12-21 21:12:40 +0000

    📦 feat: Add star count badge to HeaderAction component 🔧 chore: Update HeaderAction imports to include useEffect
    The HeaderAction component now displays a badge showing the GitHub star count, enhancing user engagement and providing social proof of the project's popularity. The use of `useEffect` and `useState` allows for dynamic fetching and updating of the star count from GitHub's API. Additionally, this update includes importing `useEffect` from React to enable this feature.

--- components/HeaderAction.tsx
+++ components/HeaderAction.tsx
@@ -15,11 +15,27 @@ import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import conffetti from "canvas-confetti";
 import { toast } from "sonner";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getOS } from "@/app/utils";
+import { Badge } from "./ui/badge";
 
 export const HeaderAction = () => {
   const [email, setEmail] = useState("");
+  const [starCount, setStarCount] = useState(0);
+
+  useEffect(() => {
+    fetch("https://api.github.com/repos/mangledbottles/screenlink-desktop")
+      .then((response) => response.json())
+      .then((data) => setStarCount(data.stargazers_count))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  const formatStarCount = (count: number) => {
+    return new Intl.NumberFormat("en-US", { notation: "compact" }).format(
+      count
+    );
+  };
+
   return (
     <span
       className="isolate inline-flex rounded-md shadow-sm"
@@ -98,6 +114,9 @@ export const HeaderAction = () => {
             className="relative -ml-px inline-flex items-center rounded-r-md bg-white/10 px-3 py-2 text-sm font-semibold text-white ring-1 ring-inset ring-gray-300 hover:bg-white/20 focus:z-10"
           >
             <GithubIcon className="mr-2 h-4 w-4" /> Star on GitHub
+            <Badge variant="secondary" className="ml-2">
+              {formatStarCount(starCount)}
+            </Badge>
           </button>
         </a>
       </div>
