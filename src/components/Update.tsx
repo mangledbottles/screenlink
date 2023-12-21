6e8c5d131244f532b7ed6f72221ad840db171c57
Dermot <contact@dermotobrien.me>
2023-12-21 21:37:57 +0000

    🌗 feat: Add dark theme support for logo in Update component
    The `Update` component in `src/components/Update.tsx` is enhanced with dark theme support. It now includes a conditional rendering of the ScreenLink logo based on the current theme. This is achieved by importing a new `screenlinkLogoDark` asset and utilizing the `useTheme` hook to determine the active theme. If the dark theme is active, the component renders the `screenlinkLogoDark`; otherwise, it displays the standard `screenlinkLogo`. This change improves the visual consistency and user experience, especially for users who prefer dark mode interfaces.

--- src/components/Update.tsx
+++ src/components/Update.tsx
@@ -1,13 +1,16 @@
 import screenlinkLogo from "../assets/screenlink.svg";
+import screenlinkLogoDark from "../assets/screenlink-dark.svg";
+import { useTheme } from "./theme-provider";
 
 export default function Update({ updateMessage }: { updateMessage: string }) {
+  const { theme } = useTheme();
   return (
     <div className="card items-center justify-center min-h-screen w-screen">
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
-            src={screenlinkLogo}
+            src={theme === "dark" ? screenlinkLogoDark : screenlinkLogo}
             alt="ScreenLink"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-300">
