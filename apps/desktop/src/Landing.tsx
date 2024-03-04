7af1d1f42002d539dd6e2d9be0119be8f845b0c4
Dermot <contact@dermotobrien.me>
2024-03-04 23:30:29 +0200

    📦 feat: Update dependencies and enhance Landing page functionality
    - Added `react-router-dom` dependency for improved navigation.
    - Enhanced Landing page with state management for device code and user account.
    - Integrated `useRecordingContext` for managing selected camera sources.
    - Implemented dynamic UI updates based on app status (update, permissions, loading).
    - Added Sentry setup for error tracking and user context.
    - Streamlined CSS with module for webcam and updated global styles for consistency.
    - Refined TailwindCSS config to match the project's structure.
    
    This update introduces essential dependency updates and significant enhancements to the Landing page, incorporating comprehensive state management and dynamic UI adjustments based on various app statuses. It improves navigation within the app through the addition of `react-router-dom`, enabling more complex routing scenarios. The integration of a recording context facilitates better management of camera sources, ensuring a more seamless user experience. Additionally, the update enhances error tracking and user analytics via Sentry, contributing to more robust application monitoring and debugging capabilities.

--- apps/desktop/src/Landing.tsx
+++ apps/desktop/src/Landing.tsx
@@ -1,60 +1,118 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import AudioSources from "./components/AudioSources";
 import CameraSources from "./components/CameraSources";
 import { NavBar } from "./components/NavBar";
 import { Recorder } from "./components/Recorder";
 import { ScreenSources } from "./components/Sources";
-import { Source } from "./utils";
+import { Account, Source, Status } from "./utils";
 import "./index.css";
+import { useRecordingContext } from "./contexts/RecordingContext";
+import { Permissions } from "./components/Permissions";
+import { Loading } from "./components/Loading";
+import Update from "./components/Update";
+import { getCurrentHub, setContext, setUser } from "@sentry/react";
+import { refreshDeviceCode } from "./App_1";
+import SignIn from "./components/SignIn";
 
 export const Landing = () => {
-  const [cameraSource, setCameraSource] = useState<MediaDeviceInfo | null>(
-    null
-  );
+  const [status, setStatus] = useState<Status | null>({
+    message: null,
+    type: null,
+  });
+  const [deviceCode, setDeviceCode] = useState<string | null>(null);
+  const [_, setAccount] = useState<Account | null>(null);
+
+  const { selectedCamera, setSelectedCamera } = useRecordingContext();
+
   const [selectedSource, setSelectedSource] = useState<Source | any | null>(
     null
   );
   const [audioSource, setAudioSource] = useState<MediaDeviceInfo | null>(null);
 
-  useMemo(() => {
-    // When cameraSource changes, notify the desktop application
-    (async () => {
-      if (cameraSource) {
-        // Assuming cameraSource is a MediaDeviceInfo object
-        const cameraSourceInfo = {
-          deviceId: cameraSource.deviceId,
-          kind: cameraSource.kind,
-          label: cameraSource.label,
-          groupId: cameraSource.groupId,
-        };
-        // Send this plain object through IPC
-        // await window.electron.showCameraWindow(true);
-        await window.electron.setUpdatedCameraSource(cameraSourceInfo);
-      }
-      //   } else {
-      // await window.electron.showCameraWindow(false);
-      //   }
-    })();
-  }, [cameraSource]);
-
-  window.electron.on("new-camera-source", (source: any) => {
-    console.log(`new cam source in landing ${source}`);
-    if (
-      source?.deviceId === cameraSource?.deviceId ||
-      (!source && !cameraSource)
-    )
-      return;
-    setCameraSource(source);
+  // Listen for the device code from the desktop application
+  window.electron.on("device-code", (code: string) => {
+    setDeviceCode(code);
   });
+
+  window.electron.on("set-window", (window: string, message: string) => {
+    console.log({ window, message, where: "here2398" });
+    setStatus({
+      type: window,
+      message,
+    });
+    // setWindowType(window);
+    // setWindowMessage(message);
+  });
+
+  useEffect(() => {
+    refreshDeviceCode().then((newDeviceCode: string) => {
+      setDeviceCode(newDeviceCode);
+    });
+
+    const getAccount = async () => {
+      const account = await window.electron.getAccount();
+      setAccount(account);
+      // Set user context in Sentry
+      setUser({
+        email: account?.user?.email,
+        name: account?.user?.name,
+        id: account?.id,
+      });
+    };
+    getAccount();
+
+    const getPreferences = async () => {
+      const preferences = await window.electron.getPreferences();
+      // Set user context in Sentry
+      setContext("Preferences", {
+        preferences,
+      });
+
+      if (
+        preferences?.find((preference) => preference.name === "error-logging")
+          ?.value === false
+      ) {
+        const client = getCurrentHub().getClient();
+        const options = client?.getOptions();
+        if (options) {
+          options.enabled = false;
+        }
+      }
+    };
+    getPreferences();
+  }, []);
+
+  if (status?.type === "update") {
+    return (
+      <Update
+        updateMessage={
+          status.message || "There is an update available for ScreenLink"
+        }
+      />
+    );
+  }
+
+  if (status?.type === "permissions") {
+    return <Permissions permissionsMessage={status?.message} />;
+  }
+
+  if (status?.type === "loading") {
+    return <Loading loadingMessage={status?.message} />;
+  }
+
+  // If the device code is not present, show the sign in component
+  if (!deviceCode) {
+    return <SignIn />;
+  }
 
   return (
     <>
-      <div className="card main bg-background text-foreground">
+      <div className="card main bg-background text-foreground p-4">
         <NavBar />
         <div className="flex flex-col space-y-4 w-4/6 mx-auto">
           <CameraSources
-            cameraSource={cameraSource}
-            setCameraSource={setCameraSource}
+            cameraSource={selectedCamera}
+            setCameraSource={setSelectedCamera}
           />
           <AudioSources
             audioSource={audioSource}
@@ -62,7 +120,7 @@ export const Landing = () => {
           />
           <Recorder
             selectedSource={selectedSource}
-            cameraSource={cameraSource}
+            cameraSource={selectedCamera}
             audioSource={audioSource}
           />
         </div>
--- apps/desktop/tailwind.config.js
+++ apps/desktop/tailwind.config.js
@@ -2,9 +2,7 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
