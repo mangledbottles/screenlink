72b28452af3ff9d332eb19b8b1a7204de66ff306
Dermot <contact@dermotobrien.me>
2024-03-07 19:48:11 +0200

    feat: add privacy policy to landing footer

--- apps/web/components/Footer.tsx
+++ apps/web/components/Footer.tsx
@@ -3,6 +3,7 @@ import { getOS } from "@/app/utils";
 import { useState } from "react";
 import { toast } from "sonner";
 import conffetti from "canvas-confetti";
+import Link from "next/link";
 
 const navigation = {
   product: [
@@ -136,6 +137,14 @@ export default function Footer() {
         </div>
         <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
           <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
+            <Link
+              href="/privacy"
+              className="text-indigo-500 hover:text-indigo-400"
+            >
+              Privacy Policy
+            </Link>
+          </p>
+          <p className="mt-8 text-xs leading-5 text-gray-400 md:mt-0">
             ScreenLink {new Date().getFullYear()}
           </p>
         </div>
