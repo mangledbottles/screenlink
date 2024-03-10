8bc008a5fc54b6ea2c972560dde2c14ccec6a919
Dermot <contact@dermotobrien.me>
2024-03-10 18:47:39 +0200

    🚀 feat: add new Pricing page implementation
    Creates a new `/pricing` page that incorporates the existing `Pricing` component, enhancing the application's user interface by providing a dedicated pricing section.
    
    - Imported necessary CSS styles to ensure the page aligns with the overall design language of the app.
    - Utilized the `Pricing` component to offer detailed pricing information, improving user experience by providing clear and accessible pricing details.

---
+++ apps/web/app/(default)/pricing/page.tsx
@@ -0,0 +1,14 @@
+/* Landing page Stlyes*/
+import "@/app/css/additional-styles/utility-patterns.css";
+import "@/app/css/additional-styles/range-slider.css";
+import "@/app/css/additional-styles/toggle-switch.css";
+import "@/app/css/additional-styles/theme.css";
+import Pricing from "@/components/pricing";
+
+export default function PricingPage() {
+  return (
+    <div className="mt-10">
+      <Pricing />
+    </div>
+  );
+}
