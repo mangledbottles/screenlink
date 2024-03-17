856a7f318bddd45dddc6edd94e8d1a83bf87d081
Dermot <contact@dermotobrien.me>
2024-03-17 18:08:25 +0200

    refactor: Remove the report dialog

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -15,9 +15,9 @@ init({
   debug: false,
   beforeSend(event) {
     // Check if it is an exception, and if so, show the report dialog
-    if (event.exception) {
-      showReportDialog({ eventId: event.event_id });
-    }
+    // if (event.exception) {
+    //   showReportDialog({ eventId: event.event_id });
+    // }
     return event;
   },
 });
